ca8ab5e81633e58206bb83f0f76ca708d83304b3
kevindarmawan <kevin.darmawan@shopee.com>
2024-12-15 19:38:07 +0700

    Initial Commit

---
+++ converter/converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  converterApp.swift
+//  converter
+//
+//  Created by Kevin Darmawan on 15/12/24.
+//
+
+import SwiftUI
+
+@main
+struct converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ converterTests/converterTests.swift
@@ -0,0 +1,17 @@
+//
+//  converterTests.swift
+//  converterTests
+//
+//  Created by Kevin Darmawan on 15/12/24.
+//
+
+import Testing
+@testable import converter
+
+struct converterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
